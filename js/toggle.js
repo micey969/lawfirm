d6d891c0d2e24222801a67c2e437476ab066ac5d
micey969 <micey_969@hotmail.com>
2016-08-28 23:24:41 -0400

    update README

---
+++ js/toggle.js
@@ -0,0 +1,7 @@
+jQuery(document).ready(function(){
+	jQuery(".menu-trigger").click(function(){
+		jQuery(".nav").slideToggle(400,function(){
+			jQuery(this).toggleClass("nav-expanded").css('display','');
+		});
+	});
+});
